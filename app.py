cc0c1941d65917bce82e69bc783a72bf6d2f3c6e
concordiasequnceplanner <concordia.sequnce.planner@gmail.com>
2026-03-07 02:49:13 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1691,5 +1691,7 @@ def api_admin_approve():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
